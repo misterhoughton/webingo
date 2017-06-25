90685a49b793c31da2a8bebeb887f7b96507cb89
misterhoughton <andy@andrewhoughton.co.uk>
2017-06-25 18:00:31 +0100

    styling change

--- index.php
+++ index.php
@@ -4,41 +4,63 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Webingo!</title>
-	<link href="https://fonts.googleapis.com/css?family=Bungee+Shade" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Bungee+Shade|Lato:300|Anonymous+Pro" rel="stylesheet">
 	<style>
 	body {
 		margin: 2em 10%;
 		text-align: center;
+		font-size: 1.5em;
+		font-family: 'Lato', sans-serif;
 	}
 	h1 {
-		margin-bottom: .25em;
+		margin: .5em 0;
 		font-family: 'Bungee Shade', cursive;
 		font-weight: normal;
-		font-size: 3.5em;
+		font-size: 2.75em;
+	}
+	button {
+		margin: .75em auto;
+		padding: .5em;
+		border: 1px solid;
+		cursor: pointer;
+		background-color: #0087FF;
+		color: #fff;
+		font-size: 1em;
+		font-family: 'Lato', sans-serif;
+	} 
+	button:hover {
+		background-color: #4CAF50;
+	}
+	button:disabled {
+		background-color: #CCC;
 	}
 	ul.grid {
 		list-style: none;
 		display: inline-block;
-		width: 280px;
+		width: 400px;
+		margin: 0;
 		padding: 0;
+		border: 5px solid #ccc;
+		font-family: 'Anonymous Pro', monospace;
 	}
-
-	ul.grid li{
-		display: inline-block;
+	ul.grid li {
 		box-sizing: border-box;
-		width: 50px;
-		height: 50px;
-		padding-top: 1em;
-		border: 1px solid pink;
-		font-family: monospace;
+		display: inline-block;
+		float: left;
+		width: 80px;
+		height: 80px;
+		padding-top: 1.15em;
+		border: 1px solid #ccc;
 	}
-
 	ul.grid li.got {
-		background-color: pink;	
+		background-color: #ccc;	
 	}
-
-	.monospaced {
-		font-family: monospace;
+	ul.grid li.webingo {
+		background-color: red;
+	}
+	.status {
+		min-height: 1.15em;
+		font-family: 'Anonymous Pro', monospace;
 	}
 	</style>
 </head>
@@ -65,7 +87,7 @@
 	</ul>
 
 	<h2 id="message"></h3>
-	<p id="status" class="monospaced"></p>
+	<p id="status" class="status"></p>
 
 	<button id="" type="button">Hit me!</button>
 	<script src="node_modules/jquery/dist/jquery.min.js"></script>
